79f40f3aca9c0efbc2bef5beeadef9e364c7a5b2
Merch Visoiu <merch@merchv.com>
2026-07-23 09:48:42 -0400

    store

--- Package.swift
+++ Package.swift
@@ -5,16 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "InAppPurchase",
+    platforms: [
+        .macOS(.v26),
+        .iOS(.v26)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "InAppPurchase",
             targets: ["InAppPurchase"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "InAppPurchase"
         ),
--- Sources/InAppPurchase/InAppPurchase.swift
+++ Sources/InAppPurchase/InAppPurchase.swift
@@ -1,2 +1,51 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  InAppPurchase.swift
+//
+//  Created on 2026-07-23.
+//
+
+import Foundation
+import Observation
+import StoreKit
+
+@MainActor @Observable public final class InAppPurchase {
+    
+    public var boughtNonConsumable: Bool = false
+    
+    public init() {
+        Task(priority: .background) {
+            for await verificationResult in Transaction.unfinished {
+                await handle(updatedTransaction: verificationResult)
+            }
+            for await verificationResult in Transaction.currentEntitlements {
+                await handle(updatedTransaction: verificationResult)
+            }
+            for await verificationResult in Transaction.updates {
+                await handle(updatedTransaction: verificationResult)
+            }
+        }
+    }
+    
+    private func handle(updatedTransaction verificationResult: VerificationResult<Transaction>) async {
+        // The code below handles only verified transactions; handle unverified transactions based on your business model.
+        guard case .verified(let transaction) = verificationResult else { return }
+
+        if let _ = transaction.revocationDate {
+            // Remove access to the product identified by `transaction.productID`.
+            // `Transaction.revocationReason` provides details about the revoked transaction.
+            guard transaction.productID == "com.merchv.podcasts.removeads" else { return }
+            let reason = transaction.revocationReason
+            print(reason)
+            self.boughtNonConsumable = false
+            await transaction.finish()
+            return
+        } else {
+            // Provide access to the product identified by transaction.productID.
+            guard transaction.productID == "com.merchv.podcasts.removeads" else { return }
+            print("transaction ID \(transaction.id), product ID \(transaction.productID)")
+                boughtNonConsumable = true
+            await transaction.finish()
+            return
+        }
+    }
+}
